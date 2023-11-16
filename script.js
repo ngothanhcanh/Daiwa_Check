594594d0fc7d11694217e537c2fe7fd9f33d9525
ngothanhcanh <ngothanhcanh03@gmail.com>
2023-11-16 09:37:07 +0700

    update

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function stop() {
     resultContainer.style.display = 'block';
     resultImage.src = 'https://www.bing.com/th/id/OGC.1469a4ad6386d6b6ab90c7b39371d00c?pid=1.7&rurl=https%3a%2f%2fimg2.thuthuatphanmem.vn%2fuploads%2f2019%2f03%2f02%2fhinh-anh-khan-gia-reo-ho-vo-tay-dep_013559420.gif&ehk=RE0Llt94%2bxsBLosoXsXDCtxmmDOE%2bUxxcnXqmGw5vEg%3d'; // Đặt đường dẫn ảnh GIF thực tế
     
-    if (so_lan_click >= 1 ) {
+    if (so_lan_click >= 2 ) {
         resultText.textContent = `Trả mày thêm ${so_lan_click} triệu`;
     }
     else {
